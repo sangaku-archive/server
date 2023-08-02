6da8643b0aa49d437fa86fd377f1966159c68c4c
gspinaci <gianmarco.spinaci@gmail.com>
2023-08-02 15:10:17 +0200

    restore cors
    Signed-off-by: gspinaci <gianmarco.spinaci@gmail.com>

--- main.go
+++ main.go
@@ -5,11 +5,18 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+
+	// Allow all origins
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowAllOrigins = true
+	corsConfig.AllowMethods = []string{"GET", "POST", "OPTIONS"}
+	r.Use(cors.New(corsConfig))
 
 	r.GET("/:manifest/manifest.json", func(c *gin.Context) {
 
